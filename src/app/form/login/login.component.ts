5d8980d19b5832930a27e041405949a4dba821ff
manishbaghel001 <manish20171999@gmail.com>
2025-05-06 01:26:45 +0530

    [chore] removed extra files

--- src/app/form/login/login.component.ts
+++ src/app/form/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
